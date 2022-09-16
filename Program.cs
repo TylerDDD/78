98163e56a9c5f09010f932a6c2525cf58ffd2b8b
Andrey <Andrey@Pochta.ru>
2022-09-16 18:57:06 +0700

    3

--- Program.cs
+++ Program.cs
@@ -1,1 +1,52 @@
-﻿
+﻿// задача номер 47.
+
+/* Console.WriteLine("Задайте количество строк двумерного массива:");
+int m = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Задайте количество столбцов двумерного массива:");
+int n = Convert.ToInt32(Console.ReadLine());
+double[,] twoDimArray = new double[m, n];
+Random rnd = new Random();
+void PrintArray(double[,] matr)
+{ for (int i = 0; i < m; i++)
+ { for (int j = 0; j < n; j++)
+ { Console.Write($"{matr[i, j]} ");}
+ Console.WriteLine();}}
+
+void FillArray(double[,] matr)
+{ for (int i = 0; i < m; i++)
+ { for (int j = 0; j < n; j++)
+ { matr[i,j] = Convert.ToDouble(rnd.Next(-100, 100)/10.0);}}}
+FillArray(twoDimArray);
+Console.WriteLine();
+PrintArray(twoDimArray); */
+
+// задача номер 50.
+
+Console.WriteLine("Введите размеры массива");
+int m = Convert.ToInt32(Console.ReadLine());
+int n = Convert.ToInt32(Console.ReadLine());
+int[,] array = new int[m, n];
+ 
+for (int i = 0; i < array.GetLength(0); i++) 
+{
+    for (int j = 0; j < array.GetLength(1); j++)
+        array [i, j] = Convert.ToInt32(new Random().Next(0,21));  
+}
+ 
+for (int i = 0; i < array.GetLength(0); i++) 
+{
+    for (int j = 0; j < array.GetLength(1); j++)
+        Console.Write(array[i,j] + "\t  ");
+        Console.WriteLine();
+}
+ 
+ Console.WriteLine("Введите координаты");
+ int a = Convert.ToInt32(Console.ReadLine());
+ int b = Convert.ToInt32(Console.ReadLine());
+ if (a>m && b>n)
+ Console.WriteLine("такого числа нет");
+ else
+ {
+ object c = array.GetValue(a,b);
+ Console.WriteLine(c);
+ }
